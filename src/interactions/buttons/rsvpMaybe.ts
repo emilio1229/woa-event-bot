e38cccaab9d211488e811df58e0e42c799e8771e
emilio1229 <andrezgerman@gmail.com>
2026-09-13 22:03:12 -0700

    Remove Maybe RSVP option

--- src/interactions/buttons/rsvpMaybe.ts
+++
@@ -1,6 +0,0 @@
-import type { ButtonInteraction } from "discord.js";
-import { handleEventRsvpButton } from "./shared.js";
-
-export async function handleRsvpMaybe(interaction: ButtonInteraction) {
-  await handleEventRsvpButton(interaction, "maybe");
-}
