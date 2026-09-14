c632f9c392dfb73aa091ef394174dbbcdc8dd8eb
emilio1229 <andrezgerman@gmail.com>
2026-09-13 22:03:18 -0700

    Remove Not Going RSVP option

--- src/interactions/buttons/rsvpNo.ts
+++
@@ -1,6 +0,0 @@
-import type { ButtonInteraction } from "discord.js";
-import { handleEventRsvpButton } from "./shared.js";
-
-export async function handleRsvpNo(interaction: ButtonInteraction) {
-  await handleEventRsvpButton(interaction, "no");
-}
